81a19d777530f6f592e2784b21b156a7d41a4dc9
MORGANJASON <420morganjason@gmail.com>
2023-03-26 19:16:02 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
-// const init = () => {
+const init = () => {
+  const inputForm = document.querySelector('form');
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+     const input = document.querySelector("#movie");
+
+     fetch(`http://localhost:3000/movies/${input.Form}`)
+  .then(response => response.json())
+  .then(data => {
+    const title = document.querySelector.childNode('#movieDetails');
+    const summary = document.querySelector.childnode('#movieDetails');
+
+    title.innerText = data.title;
+    summary.innerText = data.summary;
+  });
+
+  console.log(input.value);
+
+  //   fetch(`http://localhost:3000/movies/${input.value}`)
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log(data);
+  //         });
+  // });
+}
+  //  event.target.children[1].value
+  document.addEventListener('DOMContentLoaded', init);
+
+
 //   const inputForm = document.querySelector('form')
 
 //   inputForm.addEventListener('submit', (event) => {
@@ -40,3 +70,5 @@ const init = () => {
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+explorer.exe index.html
